47bd42216d5bb5c9ee3911f507aacabc2fdde2a7
manjitha-teshara <36306276+manjitha-teshara@users.noreply.github.com>
2018-05-28 00:14:44 +0530

    Add files via upload

---
+++ stack implimantation.py
@@ -0,0 +1,40 @@
+class Stack:
+    def __init__(self):
+        self.items=[]
+
+    def isEmpty(self):
+        return self.items==[]
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+s=Stack()
+print("Length: ",s.size())
+
+
+print(s.isEmpty())
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.isEmpty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())
+print(s.pop())
+s.push('manji')
+print(s.peek())
+print(s.pop())
+print(s.pop())
+s.push("text")
+s.push(2)
