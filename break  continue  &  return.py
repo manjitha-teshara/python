83a5bbec8c03950501d4765921f53b4e983fdc72
manjitha-teshara <36306276+manjitha-teshara@users.noreply.github.com>
2018-07-19 04:58:05 +0530

    Add files via upload

---
+++ break  continue  &  return.py
@@ -0,0 +1,35 @@
+#return
+def m():
+	for i in range(0,10):
+		for j in range(0,5):
+			if j==3:
+				return
+			else:
+				print(j)
+
+				
+#continue
+def m1():
+        for i in range(0,10):
+                for j in range(0,5):
+                        if j==3:
+                                continue
+                        else:
+                                print(j,"\n")
+
+
+
+
+#Break
+def m2():
+        for i in range(0,10):
+                for j in range(0,5):
+                        if j==3:
+                                break
+                        else:
+                                print(j,"\n")
+                                
+
+                                
+                        
+
